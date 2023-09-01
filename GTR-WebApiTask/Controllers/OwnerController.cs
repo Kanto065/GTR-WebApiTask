123db9d5aad929408dc2d7bf727b61b368eadfd6
Kanta Bhattacharjee <kanto.uv@outlook.com>
2023-09-01 11:30:11 +0600

    Add project files.

---
+++ GTR-WebApiTask/Controllers/OwnerController.cs
@@ -0,0 +1,36 @@
+﻿using GTR_WebApiTask.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GTR_WebApiTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with jwt";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with jwt";
+        }
+
+        [HttpPost]
+        [Authorize]
+        
+        public string AddUser(Users user)
+        {
+            return "User added with username" + user.Username;
+        }
+
+    }
+}
---
+++ GTR-WebApiTask/DB/taskContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GTR_WebApiTask.DB
+{
+    public class taskContext: DbContext 
+    {
+        public taskContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+
+    }
+}
